87d8f1068e101e9c803944b9f47dce41e8108c14
Paulo Fernandes Vieira <soeupfv@hotmail.com>
2024-08-28 13:12:42 -0300

    att arvore binaria

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -1,30 +1,30 @@
 import Node from "./Node.js";
 
 class BinarySearchTree {
-    
-    constructor(){
+
+    constructor() {
         this.root = null;
     }
 
-    insert(value){
-        if (this.root === null){
+    insert(value) {
+        if (this.root === null) {
             this.root = new Node(value);
-        }else{
+        } else {
             let node = this.root;
-            while(true){
-                if (value < node.value){
-                    if (node.left === null){
+            while (true) {
+                if (value < node.value) {
+                    if (node.left === null) {
                         node.left = new Node(value);
                         break;
-                    }else{
+                    } else {
                         node = node.left;
                     }
-                }else{
-                    if (node.right === null){
+                } else {
+                    if (node.right === null) {
                         node.right = new Node(value);
                         break;
                     }
-                    else{
+                    else {
                         node = node.right;
                     }
                 }
@@ -32,79 +32,171 @@ class BinarySearchTree {
         }
     }
 
-    insertRecursive(value){
-        if (this.root === null){
+    insertRecursive(value) {
+        if (this.root === null) {
             this.root = new Node(value);
-        }else{
+        } else {
             this.insertNode(this.root, value);
         }
     }
 
-    insertNode(node, value){
-        if (value < node.value){
-            if (node.left === null){
+    insertNode(node, value) {
+        if (value < node.value) {
+            if (node.left === null) {
                 node.left = new Node(value);
-            }else{
+            } else {
                 this.insertNode(node.left, value);
             }
-        }else{
-            if (node.right === null){
+        } else {
+            if (node.right === null) {
                 node.right = new Node(value);
-            }else{
+            } else {
                 this.insertNode(node.right, value);
             }
         }
     }
 
-    printNodeByLevel(){
+    printNodeByLevel() {
         let node = this.root;
-        let nodes = [node]
-        while(nodes != null){
-            let newNodes = []
-            let value = ""; 
-            for(let element of nodes){
-                value = value + element.value + " ";
-                if(element.left !== null)
-                    newNodes.push(element.left);
-                if(element.right !== null)
-                    newNodes.push(element.right);  
+        let nodes = [node];
+        while (true) {
+            let newNodes = [];
+            let values = "";
+            for (let no of nodes) {
+                values = values + no.value + " ";
+                if (no.left !== null)
+                    newNodes.push(no.left);
+                if (no.right !== null)
+                    newNodes.push(no.right);
             }
-            console.log(value);
+            console.log(values);
+            console.log("==================================")
             nodes = newNodes;
-            console.log('=====================');
-            if(nodes.length === 0){
+            if (nodes.length === 0)
                 break;
+        }
+    }
+
+    min() {
+        return this.minNode(this.root);
+    }
+
+    minNode(node) {
+        if (node == null) {
+            return null;
+        }
+
+        while (true) {
+            if (node.left !== null) {
+                node = node.left;
+            } else {
+                return node.value;
             }
         }
-        
-    }
-    maxNode(){
-        if (this.root === null){
-            this.root = new Node(value);
-        }else{
-            let node = this.root;
-            
-        }    
     }
 
-    //Insert(value)
-    //search(value)
-    //inOrder()
-    //preOrder()
-    //postOrder()
-    //min()
-    //max()
-    //remove()
+    max() {
+        return this.maxNode(this.root);
+    }
+
+    maxNode(node) {
+        if (node == null) {
+            return null;
+        }
+
+        while (true) {
+            if (node.right !== null) {
+                node = node.right;
+            } else {
+                return node.value;
+            }
+        }
+    }
+
+    inOrderTraverse(callBack) {
+        this.inOrderNode(this.root, callBack);
+    }
+
+    inOrderNode(node, callBack) {
+        if (node != null) {
+            this.inOrderNode(node.left, callBack);
+            callBack(node.value);
+            this.inOrderNode(node.right, callBack);
+        }
+    }
+
+    preOrderTraverse(callBack) {
+        this.preOrderNode(this.root, callBack);
+    }
+
+    preOrderNode(node, callBack) {
+        if (node != null) {
+            callBack(node.value);
+            this.preOrderNode(node.left, callBack);
+            this.preOrderNode(node.right, callBack);
+        }
+    }
+
+    posOrderTraverse(callBack) {
+        this.posOrderNode(this.root, callBack);
+    }
+
+    posOrderNode(node, callBack) {
+        if (node != null) {
+            this.posOrderNode(node.left, callBack);
+            this.posOrderNode(node.right, callBack);
+            callBack(node.value);
+        }
+    }
+    search(value) {
+        return this.searchNode(this.root, value);
+    }
+    searchNode(node, value) {
+        if (node == null) {
+            return false;
+        }
+        if (value === node.value) {
+            return true;
+        }
+        if (value > node.value) {
+            return this.searchNode(node.right, value);
+        }
+        if (value < node.value) {
+            return this.searchNode(node.left, value);
+        }
+    }
 }
 
-let arvore = new BinarySearchTree();
+const arvore = new BinarySearchTree();
+
+arvore.insert(50);
 arvore.insert(30);
-arvore.insert(20);
-arvore.insert(40);
-arvore.insert(5);
-arvore.insert(2);
-arvore.insert(8);
 arvore.insert(25);
-arvore.printNodeByLevel();
+arvore.insert(35);
+arvore.insert(70);
+arvore.insert(60);
+arvore.insert(55);
+arvore.insert(78);
+arvore.insert(76);
 
 
+// arvore.printNodeByLevel()
+// console.log("Min:", arvore.min())
+// console.log("Max:", arvore.max())
+
+// console.log("/////////////////////////////////////");
+
+// arvore.inOrderTraverse(value => console.log(value));
+
+// console.log("/////////////////////////////////////");
+
+// arvore.preOrderTraverse(value => console.log(value));
+
+// console.log("/////////////////////////////////////");
+
+// arvore.posOrderTraverse(value => console.log(value));
+
+console.log(arvore.search(25));
+
+
+//arvore.inOrderTraverse(value=>console.log(value));
